03900ec87898090c1c788bb30537cc0143500099
Lukasz Kreft <lukasz.kreft@vib.be>
2019-01-22 11:03:26 +0100

    basic backend server based on express

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import fs from 'fs';
+
+const app = express();
+
+const data = JSON.parse(fs.readFileSync(__dirname + "/data/sentimentdata.json", 'utf8'));
+
+app.get('/', (req, res) => res.send('Social Server'));
+
+app.get('/data', (req, res) => res.end(JSON.stringify(data)));
+
+app.get('/data/:id', (req, res) => res.end(JSON.stringify(data.filter(d => d._id == req.params.id)[0])));
+
+app.listen(4000, () => console.log(`Social Server running on port 4000`));
